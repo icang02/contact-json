ec7124de16524d5b4e5376faed9b9d470e003372
icang02 <ilmifaizan1112@gmail.com>
2022-11-06 03:49:25 +0800

    updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs'); // set view engine
 app.use(expressEjsLayouts); // Third=party middleware
